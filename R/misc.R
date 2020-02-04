8b3086212941e115470a63d36135190b93de3343
Erik Kusch <erik@i-solution.de>
2020-02-04 14:15:26 +0100

    STRUCTURE: miscellaneous functions docment with .onload

---
+++ R/misc.R
@@ -0,0 +1,15 @@
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/R-dev",
+    devtools.install.args = "",
+    devtools.name = "Erik Kusch",
+    devtools.desc.author = "Kusch, Erik <erik@i-solution.de>",
+    devtools.desc.suggests = NULL,
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+  if(any(toset)) options(op.devtools[toset])
+
+  invisible()
+}
