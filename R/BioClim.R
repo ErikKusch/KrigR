37839eb83964189e3e6e7d3d3feb0a9fe0c3a978
Erik Kusch <erik@i-solution.de>
2021-07-02 17:21:26 +0200

    download check fixed

--- R/BioClim.R
+++ R/BioClim.R
@@ -117,8 +117,8 @@ BioClim <- function(Water_Var = "volumetric_soil_water_layer_1", # could also be
       }
       if(file.exists(file.path(Dir, paste0(Var_down, "_Temporary_", Y_seq[Down_Iter], "_", M_seq[Down_Iter], ".nc")))){
         if(isTRUE(verbose)){
-          print(paste0(Var_down, " already downloaded for ", M_seq[Down_Iter], "/", Y_seq[Down_Iter]))
-        Temp_ras <- stack(file.path(Dir, paste0(Var_down, "_Temporary_", Y_seq[Down_Iter], "_", M_seq[Down_Iter], ".nc")))
+          print(paste0(Var_down, " already downloaded for ", M_seq[Down_Iter], "/", Y_seq[Down_Iter]))}
+        Temp_Ras <- stack(file.path(Dir, paste0(Var_down, "_Temporary_", Y_seq[Down_Iter], "_", M_seq[Down_Iter], ".nc")))
       }else{
         Temp_Ras <- download_ERA(
           Variable = Var_down,
