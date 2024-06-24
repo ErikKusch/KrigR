57655b2e85760bb17cf50d5ebee30ad20c401fcb
Erik Kusch <erik@i-solution.de>
2024-06-24 09:24:23 +0200

    Trial data integration for package version 1.0.0

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Coordinates of select summits in Jotunheimen national park
+#'
+#' A data frame with four rows and three columns (Summit, Lon, Lat).
+#'
+#' @format a data.frame
+#'
+"Mountains_df"
+
+#' Shapefile of boundaries of Jotunheimen national park
+#'
+#' An sf object containing a polygon describing the boundaries of Jotunheimen national park
+#'
+#' @format a sf POLYGON
+#'
+"Jotunheimen_poly"
