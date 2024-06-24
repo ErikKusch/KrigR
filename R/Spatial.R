cb21effcb3e9f430b26253da83546d08068bc3c2
Erik Kusch <erik@i-solution.de>
2024-06-24 11:10:29 +0200

    Spatial dcoumentation

--- R/Spatial.R
+++ R/Spatial.R
@@ -4,18 +4,20 @@
 #' Transform data frame with ID for querying functionality around point-loactions to SpatialPoints
 #'
 #' @param USER_df A data.frame containing geo-referenced points with Lat and Lon columns
+#'
 #' @importFrom sf st_as_sf
+#'
 #' @return An sf POINT object.
+#'
 #' @examples
-#' \dontrun{
 #' data("Mountains_df")
 #' Make.SpatialPoints(Mountains_df)
-#' }
+#'
 #' @export
 Make.SpatialPoints <- function(USER_df){
   USER_df <- data.frame(USER_df) ## attempt to catch tibbles or data.tables
   if(sum(c("Lat", "Lon") %in% colnames(USER_df)) != 2){stop("Please provide your geo-locations with a Lat and a Lon column (named exactly like such).")}
-  sf::st_as_sf(USER_df, coords = c("Lon", "Lat"), remove = FALSE)
+  st_as_sf(USER_df, coords = c("Lon", "Lat"), remove = FALSE)
 }
 ### EXTENT CHECKING ============================================================
 #' Check extent specification
@@ -23,14 +25,16 @@ Make.SpatialPoints <- function(USER_df){
 #' Try to convert user input into a SpatExtent object. Supports inputs of classes belonging to the packages raster, terra, sf, and sp
 #'
 #' @param USER_ext User-supplied Extent argument in download_ERA function call
+#'
 #' @importFrom methods getClass
 #' @importFrom terra ext
 #' @importFrom raster extent
 #' @importFrom sf st_as_sf
+#'
 #' @return A SpatExtent object.
+#'
 #' @examples
-#' \dontrun{
-#' ## raster
+#'  ## raster
 #' Check.Ext(raster::extent(c(9.87, 15.03, 49.89, 53.06)))
 #' ## terra
 #' Check.Ext(terra::ext(c(9.87, 15.03, 49.89, 53.06)))
@@ -42,7 +46,7 @@ Make.SpatialPoints <- function(USER_df){
 #' Check.Ext(sf)
 #' ## sp
 #' Check.Ext(as(sf, "Spatial"))
-#' }
+#'
 #' @export
 Check.Ext <- function(USER_ext){
   ## find package where USER_ext class originates
@@ -58,14 +62,14 @@ Check.Ext <- function(USER_ext){
 
   ## Transform into SpatExtent class
   if(package_name == "raster"){
-    OUT_ext <- terra::ext(raster::extent(USER_ext))
+    OUT_ext <- ext(extent(USER_ext))
   }
   if(package_name == "terra" | package_name == "sf"){
-    OUT_ext <- terra::ext(USER_ext)
+    OUT_ext <- ext(USER_ext)
   }
   if(package_name == "sp"){
-    USER_ext <- sf::st_as_sf(USER_ext)
-    OUT_ext <- terra::ext(USER_ext)
+    USER_ext <- st_as_sf(USER_ext)
+    OUT_ext <- ext(USER_ext)
   }
 
   ## Round digits and return
@@ -80,41 +84,45 @@ Check.Ext <- function(USER_ext){
 #'
 #' @param USER_pts An sf POINT object
 #' @param USER_buffer Size of buffer in degrees
+#'
 #' @importFrom sf st_buffer
 #' @importFrom sf st_union
+#'
 #' @return An sf polygon made up of individual square buffers around point-location input.
+#'
 #' @examples
-#' \dontrun{
 #' data("Mountains_df")
 #' User_pts <- Make.SpatialPoints(Mountains_df)
 #' Buffer.pts(User_pts, USER_buffer = 0.5)
-#' }
+#'
 #' @export
 Buffer.pts <- function(USER_pts, USER_buffer = .5){
-  sf::st_union(sf::st_buffer(USER_pts, USER_buffer, endCapStyle = "SQUARE"))
+  st_union(st_buffer(USER_pts, USER_buffer, endCapStyle = "SQUARE"))
 }
 
 ### CROPPING & MASKING =========================================================
-#' Range Masking with Edge Support
+#' Cropping & Range Masking with Edge Support
 #'
-#' Creating a mask identifying all cells in the original raster (`Base`) which are at least partially covered by the supplied shapefile (`Shape`).
+#' Cropped and masking the original SpatRaster (`BASE`) using supplied SpatExtent or shapefile (`Shape`) and retaining all pixels which are even just partially covered.
 #'
 #' @param BASE A SpatRaster within which coverage should be identified
-#' @param Shape A polygon(-collection) whose coverage of the raster object is to be found.
-#' @importFrom stars st_as_stars
-#' @importFrom sf st_cast
-#' @return A raster layer.
+#' @param Shape Either a SPatExtent or an sf polygon(-collection) whose coverage of the raster object is to be found.
+#'
+#' @importFrom terra crop
+#' @importFrom terra mask
+#'
+#' @return A SpatRaster.
+#'
 #' @examples
-#' \dontrun{
 #' data("Jotunheimen_ras")
 #' data("Jotunheimen_poly")
 #' Mask.Shape(Jotunheimen_ras, Jotunheimen_poly)
-#' }
+#'
 #' @export
 Handle.Spatial <- function(BASE, Shape){
-  ret_rast <- terra::crop(BASE, Shape)
+  ret_rast <- crop(BASE, Shape)
   if(class(Shape)[1] == "sf"){
-    ret_rast <- terra::mask(ret_rast, Shape, touches = TRUE)
+    ret_rast <- mask(ret_rast, Shape, touches = TRUE)
   }
   return(ret_rast)
 }
